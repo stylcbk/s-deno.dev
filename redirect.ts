e60de7483d845d3affff6fd79ff0436c964e1f9e
stylcbk <ponbos78@gmail.com>
2025-06-21 14:03:16 +0100

    Create redirect.ts

---
+++ redirect.ts
@@ -0,0 +1,10 @@
+const redirectUrl = "http://srv235711.hoster-test.ru/sd/login";
+
+Deno.serve(() =>
+  new Response(null, {
+    status: 302,
+    headers: {
+      Location: redirectUrl,
+    },
+  })
+);
